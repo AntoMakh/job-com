6984dedbea4d87a3010485a96482dbed45dc37f1
Chris A <chrisaccaoui@pprotonmail.com>
2025-06-15 23:22:05 +0300

    Fixed readme, created an actual page with a toolbar at the top and clickeable buttons

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Intro:
-#### Job-com, more specifically job commissions, is a website built with react used to post gigs/tasks online, as well as accept them.
+ Job-com, more specifically job commissions, is a website built with react used to post gigs/tasks online, as well as accept them.
 
 # Details:
-#### Built with React.js for the frontend, and using Google Firebase for the database and account authentication system. Will be using [Material UI](https://mui.com/material-ui/getting-started/) for the user interface theme. 
+ Built with React.js for the frontend, and using Google Firebase for the database and account authentication system. Will be using [Material UI](https://mui.com/material-ui/getting-started/) for the user interface theme. 
+
+# Development tips:
+1- Any time you want to add an element, check out the material ui page and choose what component you want to add.
+2- Make sure to run npm install to avoid errors, in case the other devs installed any libraries
--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,52 @@
 import React from "react";
-import { auth } from "./firebase";
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box'; 
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography'; 
+import Button from '@mui/material/Button';
+{/* for every ui thing youre gonna use, make sure to import it beforehand */}
+{/* Box is a div component, Typography is a fancy Text component */}
 
-function App() {  
-  return <h1>Job Board App</h1>;
+const pages = ['Browse jobs', 'Post Jobs', 'Account'];
+
+function App() {
+
+  return (
+    <AppBar position="static"> {/* the top bar thing, stays in the same spot on the screen even when scrolling */}
+      <Box sx={{ml: 5}}> {/* the sx attribute is used to write css, so sx={{ css stuff }}. ml means margin left, pd means padding down, etc pls learn diff between margin and padding */}
+        <Toolbar disableGutters>  
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="#"
+            sx={{
+              mr: 10,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            JOB-COM
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => ( 
+              <Button
+                key={page}
+                sx={{ my: 2, color: 'white', display: 'block', mx: 3 }}
+              > {/* this is like a for loop, for page in pages, page is gonna be the strings in the array. note that the actual text of the button is right under this line, whereas the key attribute of the button is used to uniquely identify the button */}
+                {page}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </Box>
+    </AppBar>
+  );
 }
+
 export default App;
