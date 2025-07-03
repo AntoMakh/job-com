21e62d2fbd48f8394967c52dface9536018d44ed
Chris A <chrisaccaoui@pprotonmail.com>
2025-07-03 23:12:00 +0300

    Added a signup page file as well as the routing for it

--- client/src/AppRouter.js
+++ client/src/AppRouter.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './Pages/HomePage';
 import LoginPage from './Pages/LoginPage';
+import SignupPage from './Pages/SignupPage';
 // every time we add a new page, we add it to the imports and the router here 
 
 function AppRouter() {
@@ -10,6 +11,7 @@ function AppRouter() {
             <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/login" element={<LoginPage />} />
+                <Route path="/signup" element={<SignupPage />} />
             </Routes>
         </BrowserRouter>
     );
---
+++ client/src/Pages/SignupPage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function SignupContainer() {
+    return(
+        <h1>this is a sign up page</h1>
+    );
+}
+
+export default SignupContainer;
